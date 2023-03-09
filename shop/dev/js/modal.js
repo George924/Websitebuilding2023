f63b03f087b0df230abacc6ff7eb216861ae6b52
Frontend JavaScript <frontendjavascript@C3-06.local>
2023-03-09 20:43:10 +0200

    curs

--- shop/dev/js/modal.js
+++ shop/dev/js/modal.js
@@ -3,10 +3,11 @@
 var id;
 $("#modalForm").on('show.bs.modal', function(event) {
     var button = $(event.relatedTarget); //Button that triggered the modal
-    const email = button.data('email') //need to find the button and get email
-    const nume = button.data('nume') //need to find the button and get name
-    $(".modal-body #email").val( email );
-    $(".modal-body #name").val( nume );
+    const produs = button.data('name') //need to find the button and get email
+    const pret = button.data('price') //need to find the button and get name
+    const description = button.data('description');
+    $(".modal-header #titleModaLabel").html( produs );
+    $(".modal-body #price").html( pret );
     id = button.attr('id');
     // var datainfo = document.getElementById(id);
      alert(JSON.stringify(button[0]));
